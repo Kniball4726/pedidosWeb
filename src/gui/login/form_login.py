f6cbcbdfdc22805619b016417b97586e87b1d18a
Gregory Rodriguez <gregory@RedKali.glrd4726ml@gmail.com>
2026-07-26 09:59:11 -0300

    Avances

--- src/gui/login/form_login.py
+++ src/gui/login/form_login.py
@@ -7,45 +7,64 @@ from src.gui.master.form_master import MasterPanel
 class App:
 
     def validar(self):
-        """usu = self.usuario.get()
+        usu = self.usuario.get().capitalize()
         passw = self.password.get()
 
         if usu == "Root" and passw == "1234":
             self.ventana.destroy()
             MasterPanel(usu)
-        elif usu != "Root":
-            messagebox.showinfo("Usuario incorrecto",message="Mensaje")
-        elif passw == "1234":
-            messagebox.showinfo("Contraseña incorrecta",message="Mensaje")
+        elif usu != "Root" and passw == "1234":
+            messagebox.showwarning(title='Mensaje',message="Usuario incorrecto")
+        elif usu == "Root" and passw != "1234":
+            messagebox.showwarning(title='Mensaje',message="Contraseña incorrecta")
+        elif usu == "" or passw == "":
+            messagebox.showwarning(title='Mensaje',message='Debe llenar los campos de usuario y contraseña')
         else:
-            messagebox.showinfo("Datos incorrectos",message="Mensaje")
-"""
+            messagebox.showwarning(title='Mensaje',message="Datos incorrectos")
+
     def __init__(self):
         self.ventana = tk.Tk()
         self.ventana.title('LogisticApp')
-        self.ventana.config(bg='black')
+        self.ventana.config(bg='#000000')
         self.ventana.resizable(False, False)
         self.ventana.withdraw()
-        utl.centrar_ventana(self.ventana, 800, 500)
+        utl.centrar_ventana(self.ventana, 700, 400)
         self.ventana.deiconify()
         logo = utl.leer_imagen("src/assets/logo2.png", (200, 200))
                 
         #frame_logo
-        frame_logo = tk.Frame(self.ventana, bd=0, width=300, relief=tk.FLAT, padx=0, pady=0, bg='black')
+        frame_logo = tk.Frame(self.ventana, bd=0, width=300, relief=tk.FLAT, padx=0, pady=0, bg='#000000')
         frame_logo.pack(side='left', expand=tk.NO, fill=tk.BOTH)
-        label = tk.Label(frame_logo, image=logo, bg='black')
+        label = tk.Label(frame_logo, image=logo, bg='#000000')
         label.place(x=0, y=0, relwidth=1, relheight=1)
 
         #frame_form
-        frame_form = tk.Frame(self.ventana, bd=0,width=300, relief=tk.FLAT, padx=0, pady=0, bg='black')
+        frame_form = tk.Frame(self.ventana, bd=0,width=300, relief=tk.FLAT, padx=0, pady=0, bg='#000000')
         frame_form.pack(side='right',expand=tk.YES,fill=tk.BOTH)
 
         #frame_form_top
-        frame_form_top = tk.Frame(frame_form, height=50,bd=0,relief=tk.SOLID,bg='black')
+        frame_form_top = tk.Frame(frame_form, height=50,bd=0,relief=tk.SOLID,bg='#000000')
         frame_form_top.pack(side='top',fill=tk.X)
-        title = tk.Label(frame_form_top,text='Inicio de Sesión',font=('Times',30),fg='white',bg='black',pady=50)
+        title = tk.Label(frame_form_top,text='Inicio de Sesión',font=('Times',30),fg='white',bg='#000000',pady=50)
         title.pack(expand=tk.YES,fill=tk.BOTH)
 
+        #frame_form_fill
+        frame_form_fill = tk.Frame(frame_form,height=50,bd=0,relief=tk.SOLID,bg='#000000')
+        frame_form_fill.pack(side='bottom',expand=tk.YES,fill=tk.BOTH)
+
+        txtUsuario=tk.Label(frame_form_fill,text="Usuario",font=('Times',14),fg='#fcfcfc',bg="#000000",anchor='nw')
+        txtUsuario.pack(fill=tk.X,padx=20,pady=10)
+        self.usuario = ttk.Entry(frame_form_fill,font=('Times',14))
+        self.usuario.pack(fill=tk.X,padx=20,pady=10)
+
+        txtPassword=tk.Label(frame_form_fill,text="Usuario",font=('Times',14),fg='#fcfcfc',bg="#000000",anchor='nw')
+        txtPassword.pack(fill=tk.X,padx=20,pady=10)
+        self.password = ttk.Entry(frame_form_fill,font=('Times',14))
+        self.password.pack(fill=tk.X,padx=20,pady=10)
+        self.password.config(show='*')
+
+        btIngreso = tk.Button(frame_form_fill,text='Ingresar',font=('Times',15, BOLD),bd=0,fg='#000000',bg='red',command=self.validar)
+        btIngreso.pack(fill=tk.X,padx=20,pady=20)
         self.ventana.mainloop()
         
 
--- src/gui/master/form_master.py
+++ src/gui/master/form_master.py
@@ -8,24 +8,12 @@ class MasterPanel:
         self.ventana.title(f"Bienvenid@, {usu}")
         w, h = self.ventana.winfo_screenwidth(), self.ventana.winfo_screenheight()
         self.ventana.geometry("%dx%d+0+0" % (w,h))
-        self.ventana.config(bg='#fcfcfc')
+        self.ventana.config(bg='#000000')
         self.ventana.resizable(False, False)
 
         logo = utl.leer_imagen("src/assets/logo2.png", (200, 200))
 
-        label = tk.Label(self.ventana, image=logo, bg="black")
+        label = tk.Label(self.ventana, image=logo, bg="#000000")
         label.place(x=0, y=0, relwidth=1, relheight=1)
 
-        """btn_cerrar = tk.Button(
-            self.ventana,
-            text="Cerrar",
-            command=self.ventana.destroy,
-            bg="#b22222",
-            fg="white",
-            relief="flat",
-            padx=10,
-            pady=5,
-        )
-        btn_cerrar.place(relx=0.98, rely=0.02, anchor="ne")
-"""
         self.ventana.mainloop()
